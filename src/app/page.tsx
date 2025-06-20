14091d200f28292b06104bf362382eea57f0d978
nnktm <s1f102402417@iniad.org>
2025-06-20 21:12:16 +0900

    CustomSettingを作成しカスタム時特有の設定をcomponentsにわけるリファクタリングを行った

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import CustomSetting from '@/components/CustomSetting';
 import DropdownList from '@/components/DropdownList';
 import type { BoardSetting, LevelKey } from '@/constants';
 import { LEVEL_KEYS, STANDARD_SETTINGS } from '@/constants';
@@ -245,109 +246,14 @@ const Home = () => {
           <DropdownList levelKey={selectedLevelKey} onChange={handleOnSelect} />
         </div>
         {selectedLevelKey === 'custom' && (
-          <div className={styles.customBoard}>
-            <div className={styles.customBoardItem}>
-              <label>
-                <strong>幅</strong>
-              </label>
-              <input
-                type="number"
-                min="1"
-                max="99"
-                value={customSetting.width}
-                onChange={(e) => {
-                  Number(e.target.value) * customSetting.height <
-                  boardSettings[selectedLevelKey].bombCount
-                    ? alert(
-                        `幅と高さの積(${Number(e.target.value) * customSetting.height})は爆弾数(${boardSettings[selectedLevelKey].bombCount})より大きくしてください`,
-                      )
-                    : Number(e.target.value) > 99
-                      ? alert(`幅は99以下にしてください（現在の値: ${Number(e.target.value)}）`)
-                      : (() => {
-                          const newCustomSetting = {
-                            ...customSetting,
-                            width: Number(e.target.value),
-                          };
-                          setCustomSetting(newCustomSetting);
-                          setUserInputBoard(
-                            Array.from({ length: newCustomSetting.height }, () =>
-                              Array.from({ length: newCustomSetting.width }, () => 0),
-                            ),
-                          );
-                        })();
-                }}
-                className={styles.textBox}
-              />
-            </div>
-            <div className={styles.customBoardItem}>
-              <label>
-                <strong>高さ</strong>
-              </label>
-              <input
-                type="number"
-                min="1"
-                max="99"
-                value={customSetting.height}
-                onChange={(e) => {
-                  Number(e.target.value) * customSetting.width <
-                  boardSettings[selectedLevelKey].bombCount
-                    ? alert(
-                        `幅と高さの積(${Number(e.target.value) * customSetting.width})は爆弾数(${boardSettings[selectedLevelKey].bombCount})より大きくしてください`,
-                      )
-                    : Number(e.target.value) > 99
-                      ? alert(`高さは99以下にしてください（現在の値: ${Number(e.target.value)}）`)
-                      : (() => {
-                          const newCustomSetting = {
-                            ...customSetting,
-                            height: Number(e.target.value),
-                          };
-                          setCustomSetting(newCustomSetting);
-                          setUserInputBoard(
-                            Array.from({ length: newCustomSetting.height }, () =>
-                              Array.from({ length: newCustomSetting.width }, () => 0),
-                            ),
-                          );
-                        })();
-                }}
-                className={styles.textBox}
-              />
-            </div>
-            <div className={styles.customBoardItem}>
-              <label>
-                <strong>爆弾数</strong>
-              </label>
-              <input
-                type="number"
-                min="1"
-                max={customSetting.width * customSetting.height - 1}
-                value={customSetting.bombCount}
-                onChange={(e) => {
-                  Number(e.target.value) > customSetting.width * customSetting.height - 1
-                    ? alert(
-                        `爆弾数はマスの数(${customSetting.width * customSetting.height})より少なくしてください（現在の値: ${Number(e.target.value)}）`,
-                      )
-                    : Number(e.target.value) < 1
-                      ? alert(`爆弾数は1以上にしてください（現在の値: ${Number(e.target.value)}）`)
-                      : (() => {
-                          const newCustomSetting = {
-                            ...customSetting,
-                            bombCount: Number(e.target.value),
-                          };
-                          setCustomSetting(newCustomSetting);
-                          setUserInputBoard(
-                            Array.from({ length: newCustomSetting.height }, () =>
-                              Array.from({ length: newCustomSetting.width }, () => 0),
-                            ),
-                          );
-                        })();
-                }}
-                className={styles.textBox}
-              />
-            </div>
-            <button onClick={handleOnSet} className={styles.setButton}>
-              更新
-            </button>
-          </div>
+          <CustomSetting
+            boardSettings={boardSettings}
+            selectedLevelKey={selectedLevelKey}
+            customSetting={customSetting}
+            setCustomSetting={setCustomSetting}
+            setUserInputBoard={setUserInputBoard}
+            handleOnSet={handleOnSet}
+          />
         )}
         <div className={styles.game}>
           <div
---
+++ src/components/CustomSetting.tsx
@@ -0,0 +1,128 @@
+import type { BoardSetting, BoardSettings, LevelKey } from '@/constants';
+import React from 'react';
+import styles from './CustomSetting.module.css';
+
+type CustomSettingProps = {
+  boardSettings: BoardSettings;
+  selectedLevelKey: LevelKey;
+  customSetting: BoardSetting;
+  setCustomSetting: (customSetting: BoardSetting) => void;
+  setUserInputBoard: (userInputBoard: number[][]) => void;
+  handleOnSet: () => void;
+};
+
+const CustomSetting: React.FC<CustomSettingProps> = ({
+  boardSettings,
+  selectedLevelKey,
+  customSetting,
+  setCustomSetting,
+  setUserInputBoard,
+  handleOnSet,
+}) => {
+  return (
+    <div className={styles.customBoard}>
+      <div className={styles.customBoardItem}>
+        <label>
+          <strong>幅</strong>
+        </label>
+        <input
+          type="number"
+          min="1"
+          max="99"
+          value={customSetting.width}
+          onChange={(e) => {
+            Number(e.target.value) * customSetting.height <
+            boardSettings[selectedLevelKey].bombCount
+              ? alert(
+                  `幅と高さの積(${Number(e.target.value) * customSetting.height})は爆弾数(${boardSettings[selectedLevelKey].bombCount})より大きくしてください`,
+                )
+              : Number(e.target.value) > 99
+                ? alert(`幅は99以下にしてください（現在の値: ${Number(e.target.value)}）`)
+                : (() => {
+                    const newCustomSetting = {
+                      ...customSetting,
+                      width: Number(e.target.value),
+                    };
+                    setCustomSetting(newCustomSetting);
+                    setUserInputBoard(
+                      Array.from({ length: newCustomSetting.height }, () =>
+                        Array.from({ length: newCustomSetting.width }, () => 0),
+                      ),
+                    );
+                  })();
+          }}
+          className={styles.textBox}
+        />
+      </div>
+      <div className={styles.customBoardItem}>
+        <label>
+          <strong>高さ</strong>
+        </label>
+        <input
+          type="number"
+          min="1"
+          max="99"
+          value={customSetting.height}
+          onChange={(e) => {
+            Number(e.target.value) * customSetting.width < boardSettings[selectedLevelKey].bombCount
+              ? alert(
+                  `幅と高さの積(${Number(e.target.value) * customSetting.width})は爆弾数(${boardSettings[selectedLevelKey].bombCount})より大きくしてください`,
+                )
+              : Number(e.target.value) > 99
+                ? alert(`高さは99以下にしてください（現在の値: ${Number(e.target.value)}）`)
+                : (() => {
+                    const newCustomSetting = {
+                      ...customSetting,
+                      height: Number(e.target.value),
+                    };
+                    setCustomSetting(newCustomSetting);
+                    setUserInputBoard(
+                      Array.from({ length: newCustomSetting.height }, () =>
+                        Array.from({ length: newCustomSetting.width }, () => 0),
+                      ),
+                    );
+                  })();
+          }}
+          className={styles.textBox}
+        />
+      </div>
+      <div className={styles.customBoardItem}>
+        <label>
+          <strong>爆弾数</strong>
+        </label>
+        <input
+          type="number"
+          min="1"
+          max={customSetting.width * customSetting.height - 1}
+          value={customSetting.bombCount}
+          onChange={(e) => {
+            Number(e.target.value) > customSetting.width * customSetting.height - 1
+              ? alert(
+                  `爆弾数はマスの数(${customSetting.width * customSetting.height})より少なくしてください（現在の値: ${Number(e.target.value)}）`,
+                )
+              : Number(e.target.value) < 1
+                ? alert(`爆弾数は1以上にしてください（現在の値: ${Number(e.target.value)}）`)
+                : (() => {
+                    const newCustomSetting = {
+                      ...customSetting,
+                      bombCount: Number(e.target.value),
+                    };
+                    setCustomSetting(newCustomSetting);
+                    setUserInputBoard(
+                      Array.from({ length: newCustomSetting.height }, () =>
+                        Array.from({ length: newCustomSetting.width }, () => 0),
+                      ),
+                    );
+                  })();
+          }}
+          className={styles.textBox}
+        />
+      </div>
+      <button onClick={handleOnSet} className={styles.setButton}>
+        更新
+      </button>
+    </div>
+  );
+};
+
+export default CustomSetting;
